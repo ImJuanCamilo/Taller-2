deaf9ca57c0c229330f8ff298bbef3245463e8df
ImJuanCamilo <jcar320@hotmail.com>
2017-05-02 15:57:55 -0500

    punto4.py

---
+++ punto4.py
@@ -0,0 +1,19 @@
+import random
+
+array =[2, 172, 11, 9, 7, 28, 22, 15, 5, 19, 36,  17]
+
+
+n = len(array)
+minimo = abs(array[0] - array[1])
+
+for i in range (n):
+  
+  for j in range (n):
+    
+    if i<j:
+      
+      minimo = abs(array[i]-array[j]) if abs(array[i] - array[j]) > minimo else minimo
+ 
+
+print array  
+print minimo
